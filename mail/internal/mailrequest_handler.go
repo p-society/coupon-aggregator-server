7bc13fcd6a9c5d6b2c148e023104734df85ada96
Soubhik Kumar Gon <b422056@iiit-bh.ac.in>
2024-03-15 02:53:10 +0530

    feat: adds mail server

---
+++ mail/internal/mailrequest_handler.go
@@ -0,0 +1,48 @@
+package internal
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/p-society/coupon-aggregator/mail/internal/models"
+)
+
+func MailRequestHandler(w http.ResponseWriter, r *http.Request) {
+	var paramInstance models.MailingParams
+
+	senderName := "Programming Society IIIT-Bh"
+	senderAddress := "sampagon738@gmail.com"
+	senderPassword := "jppxywoiaflvneue"
+
+	sender := NewGmailSender(senderName, senderAddress, senderPassword)
+
+	err := json.NewDecoder(r.Body).Decode(&paramInstance)
+
+	if err != nil {
+		jsonPayload := `{"err": "` + err.Error() + `"}`
+		fmt.Println(err)
+		w.WriteHeader(http.StatusBadRequest)
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(jsonPayload)
+		return
+	}
+
+	fmt.Println(paramInstance)
+
+	err = sender.SendEmail(paramInstance.Subject, paramInstance.Content, paramInstance.To, nil, nil, nil)
+
+	if err != nil {
+		jsonPayload := `{"err": "` + err.Error() + `"}`
+		fmt.Println(err)
+		w.WriteHeader(http.StatusBadRequest)
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(jsonPayload)
+		return
+	}
+
+	fmt.Println("Sucess")
+	json.NewEncoder(w).Encode(map[string]interface{}{
+		"message": "mail sent successfully",
+	})
+}
